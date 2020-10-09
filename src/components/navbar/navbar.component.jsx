33b301b7a65ec73edaed3c15967e66af17f9f565
Martin Abreu <martin.abreu@live.com>
2020-10-08 17:36:10 -0700

    Small nav styling change

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -9,7 +9,7 @@ import "./navbar.style.scss";
 export default function NavBar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const NAV_BAR_CENTER = {
+  const NAV_ITEMS_CENTER = {
     display: "Grid",
     placeItems: "center",
   };
@@ -17,17 +17,17 @@ export default function NavBar() {
   return (
     <Menu mode="horizontal">
       <Row className="navBa">
-        <Col span={8}>
+        <Col span={8} style={NAV_ITEMS_CENTER}>
           <h3>DRINKSTER</h3>
         </Col>
-        <Col span={8} style={NAV_BAR_CENTER}>
+        <Col span={8} style={NAV_ITEMS_CENTER}>
           <Menu mode="horizontal">
             <Menu.Item>Trending</Menu.Item>
             <Menu.Item>Categories</Menu.Item>
             <Menu.Item>About</Menu.Item>
           </Menu>
         </Col>
-        <Col span={8} style={{ display: "Grid", placeItems: "center" }}>
+        <Col span={8} style={NAV_ITEMS_CENTER}>
           <Menu mode="horizontal">
             <Menu.Item>Sign Up</Menu.Item>
             <Menu.Item>{!isLoggedIn ? "Login" : "Logout"}</Menu.Item>
